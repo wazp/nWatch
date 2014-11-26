b61185444b5d40ed31e6ce2ad596cba354ed5f3d
Patrick Waks <wazpys@gmail.com>
2014-11-26 10:45:08 -0600

    Added showing of actual time

--- src/main.c
+++ src/main.c
@@ -1,13 +1,38 @@
 #include <pebble.h>
 #include "main.h"
 
+// Background bitmap
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
+
+// Time TextLayer
+static TextLayer *s_time_layer;
 
 #define KEY_TEMPERATURE 0
 #define KEY_CONDITIONS 1
   
 static Window *s_main_window;
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[] = "00:00";
+
+  // Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true) {
+    // Use 24 hour format
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+  } else {
+    // Use 12 hour format
+    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+  }
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, buffer);
+}
 
 static void main_window_load(Window *window) {
   // Create GBitmap, then set to created BitmapLayer
@@ -15,11 +40,29 @@ static void main_window_load(Window *window) {
   s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+	
+  // Create time TextLayer
+  s_time_layer = text_layer_create(GRect(0, 105, 144, 30));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+	
+  update_time();
 }
 
 static void main_window_unload(Window *window) {
   gbitmap_destroy(s_background_bitmap); // Destroy GBitmap
   bitmap_layer_destroy(s_background_layer); // Destroy BitmapLayer
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
 }
 
 static void init() {
@@ -34,6 +77,9 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+	
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit() {
